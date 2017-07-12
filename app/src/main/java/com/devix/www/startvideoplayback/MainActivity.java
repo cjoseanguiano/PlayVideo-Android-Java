ce9c9892655852a05f7e87c880e792f690de568a
Carlos Anguiano <c.joseanguiano@gmail.com>
2017-07-12 16:05:43 -0500

    VideoView 02

--- app/src/main/java/com/devix/www/startvideoplayback/MainActivity.java
+++ app/src/main/java/com/devix/www/startvideoplayback/MainActivity.java
@@ -32,12 +32,16 @@ import android.widget.VideoView;
 
 import java.io.File;
 
-public class MainActivity extends AppCompatActivity {
+import life.knowledge4.videotrimmer.K4LVideoTrimmer;
+import life.knowledge4.videotrimmer.interfaces.OnTrimVideoListener;
+
+public class MainActivity extends AppCompatActivity implements OnTrimVideoListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private VideoView videoView;
+//    private VideoView videoViews;
     private FrameLayout frameLayout;
     private FloatingActionButton floatingActionButton;
+    private K4LVideoTrimmer mVideoTrimmer;
 
     //TODO https://code.tutsplus.com/es/tutorials/streaming-video-in-android-apps--cms-19888
 
@@ -53,14 +57,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         frameLayout = findViewById(R.id.fragmentContainer);
         floatingActionButton = findViewById(R.id.fab);
+//        videoViews = findViewById(R.id.videoNew);
+//        createVideoViewNew();
 
-        createVideoViewNew();
-        videoView.setVideoPath(videoLenovo);
-        mediaController = new MediaController(new ContextThemeWrapper(this, R.style.Theme_Design_Light));
-        mediaController.setAlwaysDrawnWithCacheEnabled(true);
-        mediaController.requestFocus();
-        floatingActionButton.setVisibility(View.INVISIBLE);
-        videoView.start();
+        mVideoTrimmer = ((K4LVideoTrimmer) findViewById(R.id.timeLine));
+        if (mVideoTrimmer != null) {
+            mVideoTrimmer.setMaxDuration(10);
+            mVideoTrimmer.setOnTrimVideoListener(this);
+//            mVideoTrimmer.setOnK4LVideoListener(this);
+            //mVideoTrimmer.setDestinationPath("/storage/emulated/0/DCIM/CameraCustom/");
+            mVideoTrimmer.setVideoURI(Uri.parse(videoLenovo));
+//            mVideoTrimmer.setVideoInformationVisibility(true);
+        }
+
+//        videoViews.setVideoPath(videoLenovo);
+//        mediaController = new MediaController(new ContextThemeWrapper(this, R.style.Theme_Design_Light));
+//        mediaController.setAlwaysDrawnWithCacheEnabled(true);
+//        mediaController.requestFocus();
+//        floatingActionButton.setVisibility(View.INVISIBLE);
+//        videoViews.start();
 
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -70,59 +85,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                floatingActionButton.setVisibility(View.VISIBLE);
-            }
-        });
-
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
-                    @Override
-                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
-                        videoView.setMediaController(mediaController);
-                        mediaController.setAnchorView(videoView);
-                    }
-                });
-            }
-        });
-
-
-        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
-            @Override
-            public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-                alertDialogBuilder.setTitle("ERROR");
-                alertDialogBuilder.setMessage("Ocurrió un error en la reproducción del video");
-                alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface qbitsChat, int which) {
-                        qbitsChat.dismiss();
-                    }
-                });
-                alertDialogBuilder.show();
-                return true;
-            }
-        });
-
-        videoView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                //Visualiza si los botones estan activos
-//                boolean isShowing = mediaController[0].isShowing();
-
-                if (videoView.isPlaying()) {
-                    methodVisible();
-                } else {
-                    methodVisible();
-                }
-
-                return false;
-            }
-        });
+//        videoViews.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//            @Override
+//            public void onCompletion(MediaPlayer mediaPlayer) {
+//                floatingActionButton.setVisibility(View.VISIBLE);
+//            }
+//        });
+//
+//        videoViews.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//            @Override
+//            public void onPrepared(MediaPlayer mp) {
+//                mp.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+//                    @Override
+//                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+//                        videoViews.setMediaController(mediaController);
+//                        mediaController.setAnchorView(videoViews);
+//                    }
+//                });
+//            }
+//        });
+//
+//
+//        videoViews.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+//            @Override
+//            public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+//                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+//                alertDialogBuilder.setTitle("ERROR");
+//                alertDialogBuilder.setMessage("Ocurrió un error en la reproducción del video");
+//                alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface qbitsChat, int which) {
+//                        qbitsChat.dismiss();
+//                    }
+//                });
+//                alertDialogBuilder.show();
+//                return true;
+//            }
+//        });
+//
+//        videoViews.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View view, MotionEvent motionEvent) {
+//                //Visualiza si los botones estan activos
+////                boolean isShowing = mediaController[0].isShowing();
+//
+//                if (videoViews.isPlaying()) {
+//                    methodVisible();
+//                } else {
+//                    methodVisible();
+//                }
+//
+//                return false;
+//            }
+//        });
 
 
     }
@@ -136,18 +151,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    public void createVideoViewNew() {
-        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) frameLayout.getLayoutParams();
-        videoView = new VideoView(this);
-        videoView.setId(R.id.videoFile);
-        videoView.setLayoutParams(params);
-        frameLayout.addView(videoView);
-
-
-        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) frameLayout.getLayoutParams();
+    @Override
+    public void getResult(Uri uri) {
 
     }
+
+    @Override
+    public void cancelAction() {
+
+    }
+
+
+//    public void createVideoViewNew() {
+//        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) frameLayout.getLayoutParams();
+//        videoViews = new VideoView(this);
+//        videoViews.setId(R.id.videoFile);
+//        videoView.setLayoutParams(params);
+//        frameLayout.addView(videoViews);
+//
+//
+//        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) frameLayout.getLayoutParams();
+//
+//    }
 
 
 }
